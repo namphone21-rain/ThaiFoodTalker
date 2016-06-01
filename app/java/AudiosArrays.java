e93e8fa3d5c5c7409018f44eb1f7a59d668b75e2
namphone21-rain <namphone21@gmail.com>
2016-06-01 09:42:14 -0400

    Update AudiosArrays.java

--- app/java/AudiosArrays.java
+++ app/java/AudiosArrays.java
@@ -9,7 +9,7 @@ package com.gohgai.thaifoodtalker;
  */
 public class AudiosArrays {
 
-    public static final int[] home_audios = {
+    public static final int[] HOME_AUDIOS = {
             R.raw.peppsi, R.raw.sprite, R.raw.dietcoke, R.raw.coke, R.raw.beerhinaken,
             R.raw.beerchaang, R.raw.beersing, R.raw.numkoad, R.raw.kowniow, R.raw.kowsoay,
             R.raw.thomyumgoong, R.raw.thomyumbhlachon, R.raw.thomkaagai, R.raw.pudthaigoong,
@@ -20,5 +20,51 @@ public class AudiosArrays {
             R.raw.kowniowmooyang, R.raw.kowniowgaiyaang, R.raw.goaythiownumthokeneurh,
             R.raw.bameegiowmoodaeng, R.raw.goaythiowgai, R.raw.goaythiowyenthapho, R.raw.goaythiowmoo
 
+    };
+
+    public static final int[] SEATING_AUDIOS = {
+        R.raw.kone, R.raw.bhai, R.raw.kaangbone, R.raw.kaanglaang, R.raw.nung, R.raw.teenai,
+            R.raw.dee,R.raw.swaddeeka, R.raw.geekoneka, R.raw.nuengkone, R.raw.seekoneka,
+            R.raw.sibkoneka, R.raw.nungteenaikadeeka, R.raw.throngnee, R.raw.throngnun,
+            R.raw.bhaikaangboneka, R.raw.bhaikaanglangka, R.raw.throngneemai, R.raw.throngnunmai
+    };
+
+    public static final int[] BILL_AUDIOS = {
+
+            R.raw.nongka_checkbinka, R.raw.nongka, R.raw.peeka, R.raw.bin, R.raw.checkbinka,
+            R.raw.kohtodenaka, R.raw.geebaadka, R.raw.kohtodenakageebaadka, R.raw.roamthibrueyungka,
+            R.raw.thib, R.raw.roam, R.raw.roamlaewka, R.raw.yung, R.raw.yungmairoamka, R.raw.maika,
+            R.raw.chaika, R.raw.chaika_roamthiblaewka, R.raw.kobkoonka
+    };
+
+    public static final int[] RESTROOM_AUDIOS = {
+
+            R.raw.hongnumyoonaika, R.raw.teenai, R.raw.hongnum, R.raw.raan, R.raw.dern,
+            R.raw.throngnun_throngnon, R.raw.throngnee_yoonee, R.raw.taangnee, R.raw.taangnun,
+            R.raw.taang, R.raw.nee, R.raw.nun, R.raw.yoodaanlungkongraan, R.raw.yoodaanlung,
+            R.raw.throngbhai, R.raw.daankwaa, R.raw.daansai, R.raw.dernthrongbhai, R.raw.yoodaankwaa,
+            R.raw.yoodaansai, R.raw.liowsai, R.raw.liow, R.raw.liowkwaa
+    };
+
+    public static final int[] ALLERGY_AUDIOS = {
+
+            R.raw.dichunpae, R.raw.pae, R.raw.pom, R.raw.dichun, R.raw.toaleurng,
+            R.raw.dichunpaetoaleurng, R.raw.nomwoa, R.raw.dichunpaenomwoa, R.raw.kowsaalee,
+            R.raw.toalisong, R.raw.shellfish, R.raw.choorode, R.raw.hom, R.raw.neurhdaeng,
+            R.raw.hed, R.raw.pollamailegleg, R.raw.carrot, R.raw.avocado, R.raw.subbharode,
+            R.raw.makeurh, R.raw.toafugyaaw, R.raw.makeurhted
+    };
+
+    public static final int[] PROTIEN_AUDIOS = {
+            R.raw.gai, R.raw.moo, R.raw.neurh, R.raw.goong, R.raw.bhlaa, R.raw.talay,
+            R.raw.hoimalaengpoo, R.raw.bhlaamueg, R.raw.bhoo, R.raw.hoilai, R.raw.towhoo
+    };
+
+    public static final int[] GRILLING_AUDIOS = {
+            R.raw.gaiyaang, R.raw.bheeggaiyaang, R.raw.nonggaiyaang, R.raw.okegaiyaang,
+            R.raw.mooyaang, R.raw.kohmooyaang, R.raw.bhaengnomyaang, R.raw.saiyaang,
+            R.raw.neurhyaang, R.raw.naemyaang, R.raw.saigrogyaang, R.raw.bhlaanilyaang,
+            R.raw.bhlaadoogyaang, R.raw.goongmaenumyaang, R.raw.bhlaachonyaang, R.raw.kaiphing,
+            R.raw.goongyaang
     };
 }
