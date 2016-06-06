b3c7d72b9f45beb41b3e56dc5e1a454b10451c76
namphone21-rain <namphone21@gmail.com>
2016-06-06 14:37:23 -0400

    Create ImagesArrays.java

---
+++ app/java/ImagesArrays.java
@@ -0,0 +1,74 @@
+package com.gohgai.thaifoodtalker;
+
+/**
+ *
+ * Storing an array of these integers in the xml file gave me the errors that
+ * when I retrieve them, their values are 0.
+ * Therefore, I decide to store integer arrays in a Java class
+ *
+ *
+ * Created by Rattanawalee Surasorn on 5/22/2016.
+ */
+public class ImagesArrays {
+
+    public static final int[] HOME_IMAGES =  {
+            R.drawable.pepsi,
+            R.drawable.sprite,
+            R.drawable.dietcoke,
+            R.drawable.coke,
+            R.drawable.beerhinaken,
+            R.drawable.beerchang,
+            R.drawable.beersing,
+            R.drawable.numkoad,
+            R.drawable.kowniow,
+            R.drawable.kowsoay,
+            R.drawable.thomyumgoong,//1
+            R.drawable.thomyumbhlachon, //2
+            R.drawable.thomkaagai, //4
+            R.drawable.padthaigoong, //5
+            R.drawable.redcurrychicken, //6
+            R.drawable.chicken_friedrice, //7
+            R.drawable.gaprowgai_kaidow, //8
+            R.drawable.gaipudmedmamoang, //10
+            R.drawable.kowmungai, //13
+            R.drawable.kowkaijiow, //15
+            R.drawable.padseeeew, //16
+            R.drawable.goaythiowpudkeemaotalay, //17
+            R.drawable.pudkanaamoogrorb, //18
+            R.drawable.yumbhlaadoogfoo, //19
+            R.drawable.gaengsombhlaachon, //20
+            R.drawable.pudkanaanummunhoigai,//
+            R.drawable.laabmoo, //21
+            R.drawable.thumthai, //3
+            R.drawable.numthokeneurh, //9
+            R.drawable.kowniowmoobhing, //12
+            R.drawable.kowniowgaiyaang,//15
+            R.drawable.goaythiownumthokeneurh, //11
+            R.drawable.bameegiowmoodaeng, //14
+            R.drawable.goaythiowgai,//16
+            R.drawable.goaythiowyenthapho,
+            R.drawable.goaythiowmoo
+
+    };
+
+    public static int[] GRILLED_IMAGES= {
+            R.drawable.grilledchicken,//<item>ไก่ย่าง</item>
+            R.drawable.grilledchickenwings,//<item>ปีกไก่ย่าง</item>
+            R.drawable.grilledchickenthighs,//<item>น่องไก่ย่าง</item>
+            R.drawable.grilledchickenbreast,// <item>อกไก่ย่าง</item>
+            R.drawable.grilledpork,//<item>หมูย่าง</item>
+            R.drawable.grillpigsneck,//<item>คอหมูย่าง</item>
+            R.drawable.grilledpigsbreast, // <item>แป้งนมย่าง</item>
+            R.drawable.grillpigsintestine, //  <item>ไส้ย่าง</item>
+            R.drawable.grilledbeef,//<item>เนื้อย่าง</item>
+            R.drawable.naemyaang,
+            R.drawable.porksausages,
+            R.drawable.grilledtilapia,
+            R.drawable.grilledcatfish,
+            R.drawable.grilledriverprawns,
+            R.drawable.grilledsnakeheadfish,
+            R.drawable.grilledeggs,
+            R.drawable.grilledshrimps,
+            R.drawable.mixgrilledpork
+    };
+}
